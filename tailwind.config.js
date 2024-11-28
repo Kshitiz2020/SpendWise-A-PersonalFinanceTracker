81e59ba8f9c17977d6d063efd628d5a03e7cd065
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-11-28 11:14:45 +0200

    firebase project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1D4ED8", // Custom blue color
+        secondary: "#9333EA", // Custom purple color
+        accent: {
+          light: "#FDE68A", // Custom yellow (light shade)
+          DEFAULT: "#F59E0B", // Default custom orange
+          dark: "#D97706", // Darker orange
+        },
+      },
+    },
   },
   plugins: [],
 };
