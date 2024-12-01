408266fb8bf51b5b5952b3996d41787a3667dd89
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-12-01 10:28:42 +0200

    react-dom installed and paths declared

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 export default App;
