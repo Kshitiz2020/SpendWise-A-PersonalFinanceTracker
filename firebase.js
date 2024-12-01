43c463d2b6f64f4d2e3077bc8124b503b60cbc5a
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-12-01 08:48:10 +0200

    firebase setup is done

--- firebase.js
+++ firebase.js
@@ -1,13 +1,8 @@
 import { initializeApp } from "firebase/app";
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getFirestore, doc, setdoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -20,4 +15,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 const analytics = getAnalytics(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider, doc, setdoc };
