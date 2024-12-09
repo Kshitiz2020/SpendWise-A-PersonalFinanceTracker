e5506b0141f0f680d1516e52a5dd12ae8371dcf9
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-12-09 19:35:46 +0200

    login form added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-function Button({ buttonLabel, onClick, isPrimary }) {
+function Button({ buttonLabel, onClick, isPrimary, disabled }) {
   return (
     <button
+      buttonLabel={buttonLabel}
       onClick={onClick}
+      disabled={disabled}
       className={`w-full px-4 py-2 mt-4 text-lg font-semibold rounded-md focus:outline-none focus:ring-4 transition-all ${
         isPrimary
           ? "bg-indigo-600 text-white hover:bg-indigo-700 focus:ring-indigo-300" // Blue Button
--- src/components/SignUpSignIn.jsx
+++ src/components/SignUpSignIn.jsx
@@ -6,88 +6,167 @@ import SignUp from "../pages/SignUp";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import { ToastContainer, toast } from "react-toastify";
+import { use } from "react";
 
 function SignUpSignIn() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [logIn, setLogin] = useState(false);
 
   // function to signupwithemail
   function signUpWithEmail() {
     //authenticate the user, or basically create a new account using email and password
-
+    setLoading(true);
     if (name != "" && password != "" && confirmPassword != "") {
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Signed up
-          const user = userCredential.user;
-          // ...
-          toast.success("You are in!");
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          toast.error(errorMessage);
-          // ..
-        });
+      if (password == confirmPassword) {
+        createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed up
+            const user = userCredential.user;
+            // ...
+            toast.success("You are in!");
+            setLoading(false);
+            setName("");
+            setEmail("");
+            setPassword("");
+            setConfirmPassword("");
+            createDoc(user);
+
+            // creating the doc with the user id as following id
+          })
+          .catch((error) => {
+            setLoading(false);
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            toast.error(errorMessage);
+            // ..
+          });
+      } else {
+        toast.error("password and confirm password doesnot match");
+      }
     } else {
+      setLoading(false);
       toast.err("All fields are mandatory,Please fill it!");
     }
   }
 
+  function createDoc(user) {
+    //make sure doc with same user id doesnt exist
+    //create doc
+  }
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600">
-      <div className="p-8 bg-white rounded-lg shadow-xl w-96">
-        <h2 className="mb-6 text-3xl font-bold text-center text-gray-800">
-          Sign Up on <span className="text-indigo-600">SpendWise</span>
-        </h2>
-        <p className="mb-4 text-xl text-center text-gray-600">
-          Manage your finances effortlessly.
-        </p>
-        <Input
-          type="text"
-          label="Full Name"
-          state={name}
-          setState={setName}
-          placeholder="Your Name"
-        />
-        <Input
-          type="text"
-          label="Email"
-          state={email}
-          setState={setEmail}
-          placeholder="YourEmail@gmail.com"
-        />
-        <Input
-          label="Password"
-          type="password"
-          state={password}
-          setState={setPassword}
-          placeholder="Your Password"
-        />
-        <Input
-          label="Confirm Password"
-          type="password"
-          state={confirmPassword}
-          setState={setConfirmPassword}
-          placeholder=" Confirm Your Password"
-        />
-        <Button
-          buttonLabel="Sign Up"
-          isPrimary={true}
-          onClick={signUpWithEmail}
-        />
-        <p className="text-center">or</p>
-        <Button buttonLabel="SignUp with Google" />
-        <p className="mt-6 text-sm text-center text-gray-600">
-          Already have an account?
-          <a href="#" className="font-medium text-indigo-600 hover:underline">
-            Log In
-          </a>
-        </p>
-      </div>
-    </div>
+    <>
+      {login ? (
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600">
+          <div className="p-8 bg-white rounded-lg shadow-xl w-96">
+            <h2 className="mb-6 text-3xl font-bold text-center text-gray-800">
+              Login on <span className="text-indigo-600">SpendWise</span>
+            </h2>
+            <p className="mb-4 text-xl text-center text-gray-600">
+              Manage your finances effortlessly.
+            </p>
+
+            <Input
+              type="text"
+              label="Email"
+              state={email}
+              setState={setEmail}
+              placeholder="YourEmail@gmail.com"
+            />
+            <Input
+              label="Password"
+              type="password"
+              state={password}
+              setState={setPassword}
+              placeholder="Your Password"
+            />
+
+            <Button
+              buttonLabel={loading ? "Loading..." : "Login"}
+              isPrimary={true}
+              onClick={signUpWithEmail}
+              disabled={loading}
+            />
+            <p className="text-center">or</p>
+            <Button
+              disabled={loading}
+              buttonLabel={loading ? "Loading..." : "SignUp with Google"}
+            />
+            <p className="mt-6 text-sm text-center text-gray-600">
+              Already have an account?
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:underline"
+              >
+                Log In
+              </a>
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600">
+          <div className="p-8 bg-white rounded-lg shadow-xl w-96">
+            <h2 className="mb-6 text-3xl font-bold text-center text-gray-800">
+              Sign Up on <span className="text-indigo-600">SpendWise</span>
+            </h2>
+            <p className="mb-4 text-xl text-center text-gray-600">
+              Manage your finances effortlessly.
+            </p>
+            <Input
+              type="text"
+              label="Full Name"
+              state={name}
+              setState={setName}
+              placeholder="Your Name"
+            />
+            <Input
+              type="text"
+              label="Email"
+              state={email}
+              setState={setEmail}
+              placeholder="YourEmail@gmail.com"
+            />
+            <Input
+              label="Password"
+              type="password"
+              state={password}
+              setState={setPassword}
+              placeholder="Your Password"
+            />
+            <Input
+              label="Confirm Password"
+              type="password"
+              state={confirmPassword}
+              setState={setConfirmPassword}
+              placeholder=" Confirm Your Password"
+            />
+            <Button
+              buttonLabel={loading ? "Loading..." : "Sign Up"}
+              isPrimary={true}
+              onClick={signUpWithEmail}
+              disabled={loading}
+            />
+            <p className="text-center">or</p>
+            <Button
+              disabled={loading}
+              buttonLabel={loading ? "Loading..." : "SignUp with Google"}
+            />
+            <p className="mt-6 text-sm text-center text-gray-600">
+              Already have an account?
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:underline"
+              >
+                Log In
+              </a>
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
