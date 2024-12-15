8a98c1d2c11282ec988f57842572ca0711042aa9
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-12-15 11:59:45 +0200

    logout function implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,34 @@ import React, { useEffect } from "react";
 import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+import { toast } from "react-toastify";
 
 function Header() {
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (user) {
+    if (!loading && user) {
       navigate("/dashboard");
     }
   }, [user, loading]);
+
   function logout() {
-    alert("logout!");
+    try {
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          toast.success("Logged Out, visit us soon!");
+          navigate("/");
+        })
+        .catch((error) => {
+          // An error happened.
+          toast.error(error.message);
+        });
+    } catch (e) {
+      toast.error(e.message);
+    }
   }
 
   return (
