0d9b7c1b646ce171bcd563ec0600f5cac43591d1
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-12-15 22:00:25 +0200

    modal added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,35 +2,42 @@ import React from "react";
 import { Card, Row } from "antd";
 import Button from "./Button";
 
-const Cards = () => (
+const Cards = ({ showExpensesModal, showIncomeModal }) => (
   <div className="p-4 mx-auto">
     <Row gutter={[16, 16]} justify="center">
+      {/* current balance */}
       <Card
-        className="bg-green-400 box-shadow min-w-[300px]"
+        className="bg-green-200  box-shadow min-w-[300px]"
         title="Available Balance"
       >
-        <strong>€0</strong>
+        <strong>€</strong>0
         <Button buttonLabel="Restore Balance" />
       </Card>
+
+      {/*Total Income */}
       <Card
-        className="bg-green-400 box-shadow min-w-[300px]"
+        className="bg-green-200  box-shadow min-w-[300px]"
         title="Total Income"
       >
-        <strong>€0</strong>
-        <Button buttonLabel="Deposit Income" />
+        <strong>€</strong>0
+        <Button buttonLabel="Deposit Income" onClick={showIncomeModal} />
       </Card>
+
+      {/*Total Expenses */}
       <Card
-        className="bg-green-400 box-shadow min-w-[300px]"
+        className="bg-green-200  box-shadow min-w-[300px]"
         title="Total expenses"
       >
-        <strong>€0</strong>
-        <Button buttonLabel="Add Expenses" />
+        <strong>€</strong>0
+        <Button buttonLabel="Add Expenses" onClick={showExpensesModal} />
       </Card>
+
+      {/* Loans/Investment */}
       <Card
-        className="bg-green-400 box-shadow min-w-[300px]"
+        className="bg-green-200  box-shadow min-w-[300px]"
         title="Loans/Investment"
       >
-        <strong>€0</strong>
+        <strong>€</strong>0
         <Button buttonLabel="Restore Balance" />
       </Card>
     </Row>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Cards from "../components/Cards";
+import { Modal } from "antd";
 
 function Dashboard() {
+  const [isExpensesModalVisible, setIsExpensesModalVisible] = useState(false);
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+
+  const showExpensesModal = () => {
+    setIsExpensesModalVisible(true);
+  };
+  const showIncomeModal = () => {
+    setIsIncomeModalVisible(true);
+  };
+  const handleExpensesCancel = () => {
+    setIsExpensesModalVisible(false);
+  };
+  const handleIncomeCancel = () => {
+    setIsIncomeModalVisible(false);
+  };
   return (
     <div>
       <Header />
-      <Cards />
+      <Cards
+        showExpensesModal={showExpensesModal}
+        showIncomeModal={showIncomeModal}
+        handleExpensesCancel={handleExpensesCancel}
+        handleIncomeCancel={handleIncomeCancel}
+      />
+      <Modal open={isIncomeModalVisible}>Income</Modal>
+      <Modal open={isExpensesModalVisible}>Expense</Modal>
     </div>
   );
 }
